1503b7cbe9116334e699a455bcca9fc714ee54f9
Manideep Dasari <manideepreddydasari2202@gmail.com>
2026-05-14 02:45:12 +0530

    Robots Variable Data

--- day1.py
+++ day1.py
@@ -1,1 +1,12 @@
-Print("day1-learning-python")
+# Robot sensor variables    
+robot_name = "manideep-Bot"
+battery_level= 85.5
+speed= 0.5
+is_moving = False
+obstacle_distance = 0.3
+
+print("Robot Name:", robot_name)
+print("Battery Level:", battery_level, "%")
+print("Speed:", speed, "m/s")
+print("Is Moving:", is_moving)
+print("Obstacle Distance:", obstacle_distance, "meters")  
